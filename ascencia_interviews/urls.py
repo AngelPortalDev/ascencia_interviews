bd8cfaa465d093e4ed86a4558cab099765f46865
AngelPortalDev <info@angel-portal.com>
2025-01-23 12:48:56 +0530

    update template

--- ascencia_interviews/urls.py
+++ ascencia_interviews/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from adminpanel.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("index/", index),
+    path("admin/", admin.site.urls),
 ]
