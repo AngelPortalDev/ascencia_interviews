d627f1e96280016838be69ecfe2fc6f80edab67a
ankita24gavas <ankita24gavas@gmail.com>
2025-02-18 16:41:52 +0530

    interview video submit question display

--- studentpanel/views/interview_process.py
+++ studentpanel/views/interview_process.py
@@ -4,6 +4,9 @@ from flask import Flask, request, jsonify
 import os
 from django.views.decorators.csrf import csrf_exempt
 from adminpanel.common_imports import CommonQuestion
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
 # from .serializers import QuestionSerializer
 # from .serializers import QuestionSerializer
 def interview_start(request):
@@ -17,30 +20,39 @@ def interview_panel(request):
 def interview_score(request):
     return render(request, "interview-score.html")
 
+
+def handle_uploaded_file(f):
+    upload_dir = os.path.join(settings.STUDENT_UPLOAD, 'uploads/student_interview/')
+    os.makedirs(upload_dir, exist_ok=True)
+    file_path = os.path.join(upload_dir, f.name)
+    with open(file_path, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return file_path
 # app = Flask(__name__)
 # @app.route('/interveiw-section/interview_video_upload',methods=['POST'])
 @csrf_exempt
 def interview_video_upload(request):
-    print(r"test")
-    # if 'file' not in request.files:
-    #     return jsonify({'error': 'No file part in the request'}), 400
-    # file = request.files['file']
-    # if file.filename == '':
-    #     return jsonify({'error': 'No selected file'}), 400
-    # filename = secure_filename(file.filename)
-    # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    # return jsonify({'message': 'File uploaded successfully'}), 200
+    # print(r"test")
+    if request.method == 'POST' and 'file' in request.FILES:
+        file = request.FILES['file']
+        file_path = handle_uploaded_file(file)
+        return JsonResponse({'message': 'File successfully uploaded', 'file_path': file_path})
+    else:
+        return JsonResponse({'error': 'No file part in the request'}, status=400)
 
 
-def interview_questions(request):
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part in the request'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return jsonify({'message': 'File uploaded successfully'}), 200
+
+# @csrf_exempt
+# def interview_questions(request):
+#     if request.method == 'POST' and 'file' in request.FILES:
+#         file = request.FILES['file']
+#         fs = FileSystemStorage()
+#         filename = fs.save(file.name, file)
+#         return JsonResponse({'message': 'File uploaded successfully', 'filename': filename}, status=200)
+#     else:
+#         return JsonResponse({'error': 'No file psddfsd'},status=500)
+
 # def index(request):
 #     return render('http://localhost:3000/home')
 
