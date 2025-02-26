9e577abde3273b738fe9b82e2c6b626f2bceea27
abdullahangelportal <Abdullah Khan>
2025-02-26 13:06:35 +0530

    Student Manager Panel: Display all students associated with a particular Student Manager

--- studentmanagerpanel/urls.py
+++ studentmanagerpanel/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls import path, include
 from studentmanagerpanel.views.dashboard_view import student_manager_dashboard
+from studentmanagerpanel.views.student_view import students_list
 
 
 urlpatterns = [
@@ -10,6 +11,9 @@ urlpatterns = [
             [
                 # Dashboards
                 path("dashboard/", student_manager_dashboard, name="studentmanagerdashboard"),
+
+                # Students
+                path("students/", students_list, name="studentmanager_students_list")
             ]
         ),
     ),
