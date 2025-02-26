b60c70626c2c43b296225de9694b7d0123b84885
abdullahangelportal <Abdullah Khan>
2025-02-26 17:46:37 +0530

    Student Count Dynamic on student manager dashboard

--- studentmanagerpanel/views/dashboard_view.py
+++ studentmanagerpanel/views/dashboard_view.py
@@ -1,5 +1,10 @@
 
 from adminpanel.common_imports import *
+from studentpanel.models.interview_process_model import Students
 
 def student_manager_dashboard(request):
-    return render(request, "dashboard.html")
+    students_count = Students.objects.filter(deleted_at__isnull=True,student_manager_email=request.user.email ).count()
+    data = {
+        'students_count': students_count,
+    }
+    return render(request, "dashboard.html", data)
