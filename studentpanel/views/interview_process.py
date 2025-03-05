61f55fa30f09dff38f8e5784072d78d0e1b6dedf
ankita24gavas <ankita24gavas@gmail.com>
2025-03-05 12:20:50 +0530

    student data and interviewplayer chnages

--- studentpanel/views/interview_process.py
+++ studentpanel/views/interview_process.py
@@ -8,25 +8,55 @@ from adminpanel.common_imports import *
 from studentpanel.models.interview_process_model import Students
 from django.core.files.storage import FileSystemStorage
 from studentpanel.models.student_interview_answer import StudentInterviewAnswers
+from studentpanel.models.interview_link import StudentInterviewLink
 
 from django.conf import settings
+from django_q.tasks import async_task
+
 # from .serializers import QuestionSerializer
 # from .serializers import QuestionSerializer
-def interview_start(request):
-    return render(request, "index.html")
+@csrf_exempt
+def interview_attend(request):
+    if request.method == 'POST':
+        data = request.POST
+        zoho_lead_id = data.get('zoho_lead_id')
+        if not zoho_lead_id:
+            return JsonResponse({"error": "Student ID is required"}, status=400)
+        try:
+            # Check if student exists
+            zoho_lead_id = base64.b64decode(zoho_lead_id).decode("utf-8")
+            # print(zoho_lead_id)
+            # Check if the lead exists in the table
+            if not StudentInterviewLink.objects.filter(zoho_lead_id=zoho_lead_id).exists():
+                return JsonResponse({"status": False, "message": "Student does not exist"}, status=404)
+
+            # Fetch student interview record
+            student = get_object_or_404(StudentInterviewLink, zoho_lead_id=zoho_lead_id)
+
+            # Check if interview link is expired
+            if student.is_expired:
+                return JsonResponse({"status": False, "message": "Interview link has expired."}, status=410)  # 410 GONE
 
 
-def interview_panel(request):
-    return render(request, "interview-panel.html") 
+            result = save_data(StudentInterviewLink, {'interview_attend': 'true','is_expired':'true'}, where={'zoho_lead_id': zoho_lead_id})
+            return JsonResponse({"status": True, "message": "Interview attendance updated successfully"})
+            
+        except Exception as e:
+            # students = get_object_or_404(Students, id=id)
+            return JsonResponse({"status": False,"message": "Page Not Found."},status=400)
+
+    return JsonResponse({"status": False, "error": "Invalid request method"}, status=405)
+# def interview_panel(request):
+#     return render(request, "interview-panel.html") 
 
 
-def interview_score(request):
-    return render(request, "interview-score.html")
+# def interview_score(request):
+#     return render(request, "interview-score.html")
 
 
 def handle_uploaded_file(file,zoho_lead_id):
 
-    upload_dir = os.path.join(settings.STUDENT_UPLOAD, 'uploads', 'interview_upload', zoho_lead_id)
+    upload_dir = os.path.join(settings.STUDENT_UPLOAD, 'uploads', 'interview_videos', zoho_lead_id)
     os.makedirs(upload_dir, exist_ok=True)
     file_path = os.path.join(upload_dir, file.name)
     with open(file_path, 'wb+') as destination:
@@ -49,6 +79,7 @@ def interview_video_upload(request):
             return JsonResponse({"error": f"Failed to decode Base64: {str(e)}"}, status=400)
 
         file_path = handle_uploaded_file(file, zoho_lead_id)
+        # async_task(studentpanel.views.interview_process.analyze_video(video_path,audio_path,))
 
         return JsonResponse({'message': 'File successfully uploaded', 'file_path': file_path})
     
@@ -92,82 +123,25 @@ def interview_questions(request):
 def student_data(request):
     if request.method == 'POST':
         data = request.POST
-        student_id = data.get('student_id')
-        if not student_id:
+        zoho_lead_id = data.get('zoho_lead_id')
+        if not zoho_lead_id:
             return JsonResponse({"error": "student id does not exists"}, status=500)
-    try:
-        decoded_bytes = base64.b64decode(student_id)
-        student_id = decoded_bytes.decode("utf-8")  # Convert bytes to string
-        student = Students.objects.get(zoho_lead_id=student_id)
-        student_data = [
-            {
-                'first_name': student.first_name,
-                'last_name': student.last_name,
-                'email_id': student.email,
-                'zoho_lead_id': student.zoho_lead_id,
-                'mobile_no':student.phone
-            }
-        ]
-        return JsonResponse({'student_data': student_data}, status=200)
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+        try:
+            decoded_bytes = base64.b64decode(zoho_lead_id)
+            zoho_lead_id = decoded_bytes.decode("utf-8")  # Convert bytes to string
+            student = Students.objects.get(zoho_lead_id=zoho_lead_id)
+            student_data = [
+                {
+                    'first_name': student.first_name,
+                    'last_name': student.last_name,
+                    'email_id': student.email,
+                    'zoho_lead_id': student.zoho_lead_id,
+                    'mobile_no':student.phone
+                }
+            ]
+            return JsonResponse({'student_data': student_data}, status=200)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
     
 
 
-@csrf_exempt
-def student_interview_answers(request):
-    # if request.method == 'POST':
-    #     data = request.POST
-    #     student_id = data.get('student_id')
-    #     if not student_id:
-    #         return JsonResponse({"error": "student id does not exists"}, status=500)
-    # try:
-    #     decoded_bytes = base64.b64decode(student_id)
-    #     student_id = decoded_bytes.decode("utf-8")  # Convert bytes to string
-    #     student = StudentInterviewAnswers.objects.get(zoho_lead_id=student_id)
-    #     student_data = [
-    #         {
-    #             'first_name': student.first_name,
-    #             'last_name': student.last_name,
-    #             'email_id': student.email,
-    #             'zoho_lead_id': student.zoho_lead_id,
-    #             'mobile_no':student.phone
-    #         }
-    #     ]
-
-    if request.method == "POST":
-        student_id = request.POST.get('student_id')
-        zoho_lead_id = request.POST.get('zoho_lead_id')
-        question_id = request.POST.get('question_id')
-        answer_text = request.POST.get('answer_text')
-        sentiment_score = request.POST.get('sentiment_score')
-        # sent_subj = request.POST.get('sent_subj')
-
-        grammar_accuracy = request.POST.get('grammar_accuracy')
-        try:
-            zoho_lead_id = base64.b64decode(zoho_lead_id).decode("utf-8")
-            question_id = base64.b64decode(question_id).decode("utf-8")
-            data_to_save = {
-                'student_id': student_id,
-                'zoho_lead_id': zoho_lead_id,
-                'question_id': question_id,
-                'answer_text': answer_text,
-                'sentiment_score': sentiment_score,
-                # 'sent_subj':"sdas",
-                'grammar_accuracy': grammar_accuracy,
-                'zoho_lead_id': zoho_lead_id
-            }
-            
-
-            result = save_data(StudentInterviewAnswers, data_to_save)
-            # print(r'result:', result)
-
-            if result['status']:
-                return JsonResponse({"status": True, "message": "Student updated successfully!"}, status=200)
-            else:
-                return JsonResponse({"status": False, "error": result.get('error', "Failed to update the student.")}, status=400)
-
-        except Exception as e:
-            return JsonResponse({"status": False, "error": str(e)}, status=500)
-
-    return JsonResponse({"status": False, "error": "Invalid request method"}, status=405)
