719c5c7ad5b83daee20fbe483aca1315b93c02ba
angel-portal-45 <you@example.com>
2025-03-13 12:14:38 +0530

    notifiction & doc_verified added

--- static/js/common.js
+++ static/js/common.js
@@ -1,7 +1,7 @@
 
 function confirmDelete(id, routeUrl) {
     Swal.fire({
-        title: 'Are you sure?',
+        title: 'Are you sure you want to delete this?',
         text: "This action cannot be undone!",
         icon: 'warning',
         showCancelButton: true,
@@ -13,3 +13,37 @@ function confirmDelete(id, routeUrl) {
         }
     });
 }
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+    
+// });
+
+// $("#updateUserProfile").on("click", function (e) {
+//     e.preventDefault();
+//     showAlerts(); 
+// });
+
+// function showAlerts() {
+//     const successMessage = document.querySelector("#success-message")?.value;
+//     const errorMessage = document.querySelector("#error-message")?.value;
+
+//     if (successMessage) {
+//         Swal.fire({
+//             title: "Success!",
+//             text: successMessage,
+//             icon: "success",
+//             confirmButtonText: "OK",
+//         });
+//     }
+
+//     if (errorMessage) {
+//         Swal.fire({
+//             title: "Error!",
+//             text: errorMessage,
+//             icon: "error",
+//             confirmButtonText: "OK",
+//         });
+//     }
+// }
