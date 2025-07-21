b3d24d108a2f4f9156a36644604dfe1f36487624
angel-portal-45 <you@example.com>
2025-07-21 19:17:01 +0530

    app file pushed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import ExpiredPage from "./components/ExpiredPage.js";
 import  InterviewSubmitted from "./components/InterviewSubmitted.js";
 import { ToastContainer } from "react-toastify";
 import Goback from './components/Goback.js'
+import StudentFaceEnrollment from "./components/StudentFaceEnrollment.js";
 
 function App() {
 
@@ -73,6 +74,7 @@ function App() {
           <Route path="/questions"  element={<ProtectedRoute element={<Questions/>} isAllowed={audioVideoAccepted && !isExamSubmitted} redirectPath="/permissions"/>}/>
          {/* Add Question Router Here */}
           <Route path="*" element={<NotFound />} />
+          <Route path="/studentfaceenrollment"  element={<ProtectedRoute element={<StudentFaceEnrollment/>} isAllowed={audioVideoAccepted && !isExamSubmitted} redirectPath="/permissions"/>}/>
           <Route path="/interviewsubmitted" element={<InterviewSubmitted/>}/>
           <Route path="/expired"  element={<ExpiredPage/>}/>
           <Route path="/goback" element={<Goback/>}/>
