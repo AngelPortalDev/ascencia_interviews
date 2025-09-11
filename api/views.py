d5029ee1aed3be0ff2c3836626ef5ca0880e0fd0
vaibhav-angel <vaibhav@angel-portal.com>
2025-09-11 21:12:46 +0530

    setting changes and comment mail changes

--- api/views.py
+++ api/views.py
@@ -1035,8 +1035,8 @@ def process_document(request):
                                 </body>
                             </html>
                             """,
-                            recipient=["vaibhav@angel-portal.com"],
-                            # recipient=[student_email]
+                            # recipient=["vaibhav@angel-portal.com"],
+                            recipient=[student_email]
                         )
         
 
@@ -1114,8 +1114,8 @@ def process_document(request):
                                     </body>
                                     </html>
                                     """,
-                                    # recipient=[student_manager_email]
-                                    recipient=["vaibhav@angel-portal.com"],  # Replace with actual student manager email
+                                    recipient=[student_manager_email]
+                                    # recipient=["vaibhav@angel-portal.com"],  # Replace with actual student manager email
                                     # cc=["admin@example.com"],  # Optional
                                 )
 
