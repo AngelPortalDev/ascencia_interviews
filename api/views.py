03dfcca87d13078373911b2043bc5c7b7ed55abd
vaibhav-angel <vaibhav@angel-portal.com>
2025-10-10 18:18:38 +0530

    mail replace for live

--- api/views.py
+++ api/views.py
@@ -1035,9 +1035,9 @@ def process_document(request):
                                 </body>
                             </html>
                             """,
-                            recipient=["vaibhav@angel-portal.com"],
-                            # recipient=[student_email],
-                            # reply_to=[student_manager_email]  # ✅ Ensures replies go to student manager too
+                            # recipient=["vaibhav@angel-portal.com"],
+                            recipient=[student_email],
+                            reply_to=[student_manager_email]  # ✅ Ensures replies go to student manager too
                         )
         
 
@@ -1116,8 +1116,8 @@ def process_document(request):
                                     </body>
                                     </html>
                                     """,
-                                    # recipient=[student_manager_email]
-                                    recipient=["vaibhav@angel-portal.com"],  # Replace with actual student manager email
+                                    recipient=[student_manager_email]
+                                    # recipient=["vaibhav@angel-portal.com"],  # Replace with actual student manager email
                                     # cc=["admin@example.com"],  # Optional
                                 )
 
@@ -1339,8 +1339,8 @@ def send_interview_reminders(zoho_lead_id):
         # start = student.start_time.strftime('%d %B %Y - %I:%M%p') + " (Europe/Malta)"
         # end = student.expires_at.strftime('%d %B %Y - %I:%M%p') + " (Europe/Malta)"
         # to_email = [getattr(student_obj, 'email', 'vaibhav@angel-portal.com')]
-        to_email = ["vaibhav@angel-portal.com"]
-        # to_email = [student_email]
+        # to_email = ["vaibhav@angel-portal.com"]
+        to_email = [student_email]
         subject = "sent reminder  for student"
         from_email =""
 
@@ -1463,8 +1463,8 @@ def send_interview_reminders(zoho_lead_id, reminder_type="24h"):
         subject = f"Interview reminder - Expires in {reminder_display}"
         text_content = f"Reminder: Interview for {student_name} expires in {reminder_display}. Visit: {interview_url}"
 
-        to_email = ["vaibhav@angel-portal.com"]
-        # to_email = [student_email]
+        # to_email = ["vaibhav@angel-portal.com"]
+        to_email = [student_email]
 
         # You can customize the HTML based on reminder_type
 
