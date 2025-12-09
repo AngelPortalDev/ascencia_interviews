cd47f6750dec680bb0fce08283f433c899a459b7
vaibhav-angel <vaibhav@angel-portal.com>
2025-12-09 13:45:42 +0530

    brandin logo

--- adminpanel/helper/email_branding.py
+++ adminpanel/helper/email_branding.py
@@ -9,21 +9,21 @@ def get_email_branding(crm_id: str):
     BRANDING = {
         # --- Ascencia (first two CRM IDs) ---
         "771809603": {
-            "logo_url": "https://dev.ascencia-interview.com/static/img/email_template_icon/ascencia_logo.png",
+            "logo_url": "https://ascencia-interview.com/static/img/email_template_icon/ascencia_logo.png",
             "company_name": "Ascencia Malta"
         },
         "771661420": {
-            "logo_url": "https://dev.ascencia-interview.com/static/img/email_template_icon/ascencia_logo.png",
+            "logo_url": "https://ascencia-interview.com/static/img/email_template_icon/ascencia_logo.png",
             "company_name": "Ascencia Malta"
         },
 
         # --- College De Paris (CDP India / ) ---
         "755071407": {
-            "logo_url": "https://dev.ascencia-interview.com/static/img/email_template_icon/cdp_india_logo.png",
+            "logo_url": "https://ascencia-interview.com/static/img/email_template_icon/cdp_india_logo.png",
             "company_name": "College De Paris"
         },
         "759439531": { #Intl
-            "logo_url": "https://dev.ascencia-interview.com/static/img/email_template_icon/cdp_india_logo.png",
+            "logo_url": "https://ascencia-interview.com/static/img/email_template_icon/cdp_india_logo.png",
             "company_name": "College De Paris"
         }
     }
