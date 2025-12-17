9316c421845faaa7e9be7a68dbe93f633b0fbbb3
vaibhav-angel <vaibhav@angel-portal.com>
2025-12-17 12:48:10 +0530

    setting file add

--- ascencia_interviews/settings.py
+++ ascencia_interviews/settings.py
@@ -205,7 +205,7 @@ LOGIN_URL = '/login'
 
 # ADMIN_BASE_URL = "https://bb91-103-71-112-194.ngrok-free.app"
 
-ADMIN_BASE_URL = "https://ascencia-interview.com"
+ADMIN_BASE_URL = os.environ["ADMIN_BASE_URL"]
 
 BUNNY_STREAM_API_KEY = os.getenv("BUNNY_STREAM_API_KEY")
 BUNNY_STREAM_LIBRARY_ID = os.getenv("BUNNY_STREAM_LIBRARY_ID")
@@ -267,10 +267,7 @@ Q_CLUSTER = {
 }
 
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://ascencia-interview.com",
-    "https://bb91-103-71-112-194.ngrok-free.app",
-]
+CSRF_TRUSTED_ORIGINS = os.environ["CSRF_TRUSTED_ORIGINS"]
 
 CSRF_COOKIE_SECURE = True 
 CSRF_COOKIE_SAMESITE = 'None'
