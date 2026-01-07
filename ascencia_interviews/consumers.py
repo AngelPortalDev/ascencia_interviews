2b13b62d5bc8ceecbca20221f6eb9d0b41d46d5a
angel-portal-45 <you@example.com>
2026-01-07 12:27:52 +0530

    logged added

--- ascencia_interviews/consumers.py
+++ ascencia_interviews/consumers.py
@@ -56,7 +56,7 @@ import logging
 logger = logging.getLogger('zoho_webhook_logger')
 
 ASSEMBLY_API_KEY = "6af9609039af426f822ac6a728aa94b3"
-logger.info(" AssemblyAI API Key set", ASSEMBLY_API_KEY is not None)
+logger.info("AssemblyAI API Key set: %s", bool(ASSEMBLY_API_KEY))
 
 class TranscriptionConsumer(AsyncWebsocketConsumer):
     logger.info(" TranscriptionConsumer initialized")
